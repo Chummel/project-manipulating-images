8c60e141ea4ba5c37dbf35be2ecd746723f683da
Colin Hummel <chummel4@mail.valenciacollege.edu>
2016-04-15 20:48:05 -0400

    syntax

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -17,7 +17,7 @@ for filename in os.listdir('.'):
 	width, height = im.size
 
 	if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-		if width > height
+		if width > height:
 			height = int((SQUARE_FIT_SIZE / width) * height)
 			width = SQUARE_FIT_SIZE
 		else:
